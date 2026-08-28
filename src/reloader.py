8fb6da2b1447e0298cf2cd2e783fbb311c07d993
aspenle <aspenle@k1r3p9.42mulhouse.fr>
2026-08-28 16:49:53 +0200

    score + lvl in hud

--- src/reloader.py
+++ src/reloader.py
@@ -1,81 +1,32 @@
-from src.visualizer.visualizer import *
+from typing import Callable
 
 
-def draw_hud(self, target: pygame.surface.Surface, offset: Pos2D):
-    font = self.get_font(MainData.cell_size * 3)
-    start_score = (
-        Pos2D(
-            len(self.pacmap.cells) - 1 - 4,
-            len(self.pacmap.cells[0]),
-        )
-        * MainData.cell_size
-        * 3
-    ) + offset
-    draw_text_multiline(
-        target,
-        [char for char in "lives"],
-        *start_score,
-        font,
-        block_spacing=MainData.cell_size * 3,
-    )
-    if self.pacmap.pacman.lives <= 6:
-        for x in range(self.pacmap.pacman.lives - 1):
-            pos = (
-                Pos2D(
-                    len(self.pacmap.cells) - 1 - x,
-                    len(self.pacmap.cells[0]) + 1,
-                )
-                * 3
-                + (0.75, 0.75)
-            ) * MainData.cell_size
-            target.blit(self.pacmap.pacman.raw_image, pos + offset)
-    else:
-        offset_x = 2
-        pos = (
-            Pos2D(
-                len(self.pacmap.cells) - 1 - offset_x,
-                len(self.pacmap.cells[0]) + 1,
-            )
-            * 3
-            + (0.75, 0.75)
-        ) * MainData.cell_size + offset
-        target.blit(self.pacmap.pacman.raw_image, pos)
-        draw_text_multiline(
-            target,
-            ["X", str(self.pacmap.pacman.lives - 1)],
-            pos.x + MainData.cell_size * 3,
-            pos.y,
-            font,
-            block_spacing=MainData.cell_size * 3,
-        )
-    right_top = (
-        Pos2D(
-            0.25,
-            len(self.pacmap.cells[0]),
-        )
-        * MainData.cell_size
-        * 3
-    ) + offset
-    draw_text_multiline(
-        target,
-        [char for char in "Score"],
-        *right_top,
-        font,
-        block_spacing=MainData.cell_size * 3,
-    )
-    draw_text_multiline(
-        target,
-        [char for char in f"{self.pacmap.score:05}"],
-        *(right_top + (0, MainData.cell_size * 3)),
-        font,
-        block_spacing=MainData.cell_size * 3,
-        color=(pygame.color.THECOLORS["yellow"]),
-    )
+from src.visualizer.visualizer import  Visualizer
+import ast
+import inspect
+import textwrap
+imported = Visualizer.draw_hud
 
+def copy_func(src, dest):
+    src_source = textwrap.dedent(inspect.getsource(src))
+    dest_source = textwrap.dedent(inspect.getsource(dest))
+    src_tree = ast.parse(src_source)
+    dest_tree = ast.parse(dest_source)
+    src_func = src_tree.body[0]
+    dest_func = dest_tree.body[0]
+    # Copy the source function's body into the destination.
+    dest_func.body = src_func.body
+
+    print(ast.unparse(dest_tree))
+
+
+def my_copy():
+    pass
 
 def replace(globals: dict):
-    print("\033[2D\033[K", end="", flush=True)
-    Visualizer.draw_hud.__code__ = draw_hud.__code__
+
+    copy_func(imported, my_copy)
+    imported.__code__ = my_copy.__code__
 
     # src.ai.training.evaluate.__code__ = evaluate.__code__
     # globals["MainData"].cell_size =8
